d3df0dd8fa774a546e4c229259db02dbadf6b0eb
iansseijelly <iansseijelly@berkeley.edu>
2022-10-05 20:53:47 -0700

    First edition finished!

--- apollo.py
+++ apollo.py
@@ -1,7 +1,11 @@
+from ast import parse
 from pickle import FALSE
 import yaml
 import boolean
 import random
+from functools import reduce
+
+from schemdraw.parsing import logicparse
 
 algebra = boolean.BooleanAlgebra()
 
@@ -11,19 +15,13 @@ def read_config():
     return read_dict
 
 config_dict = read_config()
-symbols_pool = ['a','b','c','d','e']
-
-def main_loop():
-    # This outer loop controls the number of trials
-    for _ in range(config_dict['trials']):
-        simply_input = generate_input()
-        dumbly_input = add_dummy(simply_input)
+symbols_pool = ['a','b','c','d','e','f','g','h']
 
 # This function generates the most simplified input
 def generate_input():
     symbols = symbols_pool[:config_dict['input_num']]
 
-        #randomly negating the valid inputs in this layer
+    #randomly negating the valid inputs in this layer
     for i in range(len(symbols)):
         rand_i = random.randint(0,1)
         if rand_i:
@@ -46,39 +44,91 @@ def generate_input():
 
 #This function generates the dummy-added inputs
 def add_dummy(simple):
+    candidates = [rev_annihilator, rev_complementation, rev_absorption, rev_elimination]
     symbols = symbols_pool[config_dict['input_num']:config_dict['dummy']+ config_dict['input_num']]
     for i in range(len(symbols)):
+        symbol = symbols[i]
         rand_i = random.randint(0,3)
-        # Annihilator
-        if rand_i == 0:
-            rand_j = random.randint(0,1)
-            if rand_j:
-                simple += f"|({symbols[i]}&{algebra.FALSE})"
-            else:
-                simple += f"&({symbols[i]}|{algebra.TRUE})"
-        # Complementation
-        if rand_i == 1:
-            rand_j = random.randint(0,1)
-            if rand_j:
-                simple += f"|({symbols[i]}&~{symbols[i]})"
-            else:
-                simple += f"&({symbols[i]}|~{symbols[i]})"
-        # Absorption
-        if rand_i == 2:
-            rand_j = random.randint(0,1)
-            if rand_j:
-                simple += f"&({simple}|{symbols[i]})"
-            else:
-                simple += f"|({simple}&{symbols[i]})"
-        # Elimination
-        if rand_i == 3:
-            rand_j = random.randint(0,1)
-            if rand_j:
-                simple = f"{simple}&{symbols[i]}|{simple}&~{symbols[i]}"
-            else:
-                simple = f"({simple}|{symbols[i]})&({simple}&~{symbols[i]})"
-    print(simple)
+        simple = candidates[rand_i](simple, symbol)
     return simple
 
+# Annihilator
+def rev_annihilator(simple, symbol):
+    rand_j = random.randint(0,1)
+    if rand_j:
+        simple += f"|({symbol}&{algebra.FALSE})"
+    else:
+        simple += f"&({symbol}|{algebra.TRUE})"
+    return simple
+
+# Complementation
+def rev_complementation(simple, symbol):
+    rand_j = random.randint(0,1)
+    if rand_j:
+        simple += f"|({symbol}&~{symbol})"
+    else:
+        simple += f"&({symbol}|~{symbol})"
+    return simple
+
+# Absorption
+def rev_absorption(simple, symbol):
+    rand_j = random.randint(0,1)
+    if rand_j:
+        simple += f"&({simple}|{symbol})"
+    else:
+        simple += f"|({simple}&{symbol})"
+    return simple
+
+# Elimination
+def rev_elimination(simple, symbol):
+    rand_j = random.randint(0,1)
+    if rand_j:
+        simple = f"{simple}&{symbol}|{simple}&~{symbol}"
+    else:
+        simple = f"({simple}|{symbol})&({simple}|~{symbol})"
+    return simple
+
+def test_transform(input, symbol, func):
+    output = func(input, symbol)
+    qwq = algebra.parse(input).simplify()
+    print(output)
+    qaq = algebra.parse(output).simplify()
+    if qwq == qaq:
+        print("the two are equal!")
+    else:
+        print("NAH")
+    print(qwq)
+    print(qaq)
+
+def de_morgan(dumb_input):
+    for _ in range(config_dict['de_morgan']):
+        parsed = algebra.parse(dumb_input)
+        print(f'parsed is \n {parsed.pretty()}')
+        lst = [boolean.NOT(i) for i in parsed.args]
+        qwq = reduce(parsed.dual, lst)
+        qwq = boolean.NOT(qwq)
+        print(f'qwq is \n {qwq.pretty()}')
+        return qwq.__str__()
+
+def main_loop():
+    # This outer loop controls the number of trials
+    ret_set = set()
+    for _ in range(config_dict['trials']):
+        simply_input = generate_input()
+        dumbly_input = add_dummy(simply_input)
+        output = de_morgan(dumbly_input)
+        print(f"dumb is :{dumbly_input}")
+        print(f"output is :{output}")
+        ret_set.add(output)
+    ret_list = list(ret_set)
+    # print(ret_list)
+    # print(ret_list[0])
+    # with logicparse(ret_list[0], outlabel='$f$') as d:
+    #     d.save('circuit.svg')
+    #     return ret_set
+    return ret_list
+
+
 main_loop()
+# test_transform("a&b", "c", rev_elimination)
 
