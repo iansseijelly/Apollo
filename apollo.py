4f055f896d312cf4247fd38c667d75b4c921f45c
Chengyi Zhang <iansseijelly@berkeley.edu>
2022-10-12 14:13:00 -0700

    version 1.0.1 Added drawing and internal checker

--- apollo.py
+++ apollo.py
@@ -1,10 +1,11 @@
-from ast import parse
-from pickle import FALSE
+import re
 import yaml
 import boolean
 import random
 from functools import reduce
+import os
 
+import schemdraw 
 from schemdraw.parsing import logicparse
 
 algebra = boolean.BooleanAlgebra()
@@ -65,27 +66,27 @@ def rev_annihilator(simple, symbol):
 def rev_complementation(simple, symbol):
     rand_j = random.randint(0,1)
     if rand_j:
-        simple += f"|({symbol}&~{symbol})"
+        simple = f"({simple})|({symbol}&~({symbol}))"
     else:
-        simple += f"&({symbol}|~{symbol})"
+        simple = f"({simple})&({symbol}|~({symbol}))"
     return simple
 
 # Absorption
 def rev_absorption(simple, symbol):
     rand_j = random.randint(0,1)
     if rand_j:
-        simple += f"&({simple}|{symbol})"
+        simple = f"({simple})&({simple}|{symbol})"
     else:
-        simple += f"|({simple}&{symbol})"
+        simple = f"({simple})|({simple}&{symbol})"
     return simple
 
 # Elimination
 def rev_elimination(simple, symbol):
     rand_j = random.randint(0,1)
     if rand_j:
-        simple = f"{simple}&{symbol}|{simple}&~{symbol}"
+        simple = f"({simple}&{symbol})|({simple}&~({symbol}))"
     else:
-        simple = f"({simple}|{symbol})&({simple}|~{symbol})"
+        simple = f"({simple}|{symbol})&({simple}|~({symbol}))"
     return simple
 
 def test_transform(input, symbol, func):
@@ -103,32 +104,69 @@ def test_transform(input, symbol, func):
 def de_morgan(dumb_input):
     for _ in range(config_dict['de_morgan']):
         parsed = algebra.parse(dumb_input)
-        print(f'parsed is \n {parsed.pretty()}')
         lst = [boolean.NOT(i) for i in parsed.args]
         qwq = reduce(parsed.dual, lst)
         qwq = boolean.NOT(qwq)
-        print(f'qwq is \n {qwq.pretty()}')
         return qwq.__str__()
 
 def main_loop():
     # This outer loop controls the number of trials
-    ret_set = set()
+    ans_list = []
+    ret_list = []
     for _ in range(config_dict['trials']):
         simply_input = generate_input()
+        # print(f"the simply_input is: {simply_input}")
         dumbly_input = add_dummy(simply_input)
+        # print(f"the dumbly_input is: {dumbly_input}")
         output = de_morgan(dumbly_input)
-        print(f"dumb is :{dumbly_input}")
-        print(f"output is :{output}")
-        ret_set.add(output)
-    ret_list = list(ret_set)
+        # print(f"output is :{output}")
+        if output not in ret_list:
+            ret_list.append(output)
+            ans_list.append(simply_input)
     # print(ret_list)
     # print(ret_list[0])
-    # with logicparse(ret_list[0], outlabel='$f$') as d:
-    #     d.save('circuit.svg')
-    #     return ret_set
+    internal_checker(ans_list, ret_list)
+
+    if config_dict['print']:
+        for i in range(len(ret_list)):
+            print(f'the {i}th answer is: {ans_list[i]}')
+            print(f'the {i}th output is: {ret_list[i]}')
+
+    if config_dict['drawing']:
+        dir = 'gen'
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
+        for i in range(len(ret_list)):
+            expr = ret_list[i]
+            expr_sub = expr.replace("0", "FALSE")
+            expr_sub = expr_sub.replace("1", "TRUE")
+            with logicparse(expr_sub, gateH = 1.1, outlabel="output") as d:
+                d.save(f'gen/circuit{i}.svg')
+
     return ret_list
 
+def draw(expr):
+    qwq = algebra.parse(expr)
+    qaq = qwq.simplify()
+    print(qwq)
+    print(qaq)
+    print(qwq.pretty())
+    print(qaq.pretty())
+    expr_sub = expr.replace("0", "FALSE")
+    expr_sub = expr_sub.replace("1", "TRUE")
+    with logicparse(expr_sub, outlabel="$out$") as d:
+        d.save('gen/test_circuit.svg')
+
+def internal_checker(ans_list, ret_list):
+    assert len(ans_list) == len(ret_list), f"their length doesn't match, with anslength {len(ans_list)}, retlength {len(ret_list)}"
+    for i in range(len(ans_list)):
+        output = ret_list[i]
+        output_simplified = algebra.parse(output).simplify()
+        ans = ans_list[i]
+        if ans != output_simplified:
+            print(f"caution! unmatching simplified output {output_simplified} and answer {ans}")
 
 main_loop()
 # test_transform("a&b", "c", rev_elimination)
 
+# draw("~a|~b")
